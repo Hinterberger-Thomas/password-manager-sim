433e8923182407f63b62126a238d49f6ceabb44a
Hinterberger-Thomas <[thomas.hinterberger.eu@gmail.com]>
2021-04-04 22:24:34 +0200

    init some prep stat

--- database/ins.go
+++ database/ins.go
@@ -12,7 +12,7 @@ type DB struct {
 	client *sql.DB
 }
 
-func Init_db() {
+func Init_db() *DB {
 	db, err := sql.Open("mysql", "mysql:mysql@/datadb")
 	if err != nil {
 		fmt.Println(err)
@@ -21,12 +21,31 @@ func Init_db() {
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
+	return &DB{
+		client: db,
+	}
 }
 
-func (db *DB) InsertAccount(accNam string) (bool, error) {
-	return false, nil
+func (db *DB) InsertAccount(accNam string, ) (int64, error) {
+	stmt, e := db.client.Prepare("insert into data(id, account, password) values (?, ?, ?)")
+	SELECT * FROM permlog WHERE max(id)
+	if e != nil {
+		return -1, e
+	}
+
+	//execute
+	res, e := stmt.Exec("5", "Post five", "Contents of post 5")
+	if e != nil {
+		return -1, e
+	}
+	id, e := res.LastInsertId()
+	if e != nil {
+		return -1, e
+	}
+
+	return id, nil
 }
 
-func (db *DB) RemoveAccount(accNam string) (bool, error) {
-	return false, nil
+func (db *DB) RemoveAccount(accNam string) (int64, error) {
+	return 0, nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/mdempsky/unconvert v0.0.0-20200228143138-95ecdbfc0b5f // indirect
 	github.com/tsenart/deadcode v0.0.0-20160724212837-210d2dc333e9 // indirect
+	github.com/walle/lll v1.0.1 // indirect
 	golang.org/x/tools v0.1.0 // indirect
 )
